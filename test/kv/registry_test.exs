7efaf508a4ee80b065dba8607bd01f00f5efcab3
romenigld <romenigld@gmail.com>
2018-11-21 19:40:19 +0100

    Chapter 6 - ETS - tests don't pass

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,9 +1,9 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
 
@@ -21,6 +21,9 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -30,6 +33,9 @@ defmodule KV.RegistryTest do
 
     # Stop the bucket with non-normal reason
     Agent.stop(bucket, :shutdown)
+    
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
